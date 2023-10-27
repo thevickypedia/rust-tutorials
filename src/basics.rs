68e55a7dfd25a3df6f8b8514299a1035f3c13c5e
thevickypedia <vignesh.sivanandarao@gmail.com>
2023-10-26 21:23:28 -0500

    Arguments and looping basics

--- src/basics.rs
+++ src/basics.rs
@@ -1,7 +1,23 @@
-use std::net::SocketAddr;
+use std::env;
 
 fn main() {
-    println!("Hello, world!");
-    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
-    println!("{}", addr.to_string());
+    println!("Hello, World!");
+    args()
+}
+
+fn args() {
+    // Either use 'cargo run' or 'rustc src/basics.rs && ./basics'
+    println!("\nCommandLine argument section");
+    let args: Vec<String> = env::args().collect();
+    let program_name = &args[0];
+    println!("Invoker: '{}'", program_name);
+    looping()
+}
+
+
+fn looping() {
+    println!("\nLooping section");
+    for i in [1, 2, 3, 4] {
+        println!("{}", i);
+    }
 }
