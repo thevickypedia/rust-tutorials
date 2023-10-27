82c9d9b3dde9a9a4c22007d0755459be01014c81
thevickypedia <vignesh.sivanandarao@gmail.com>
2023-10-26 21:38:55 -0500

    Command line arguments and conditions

--- src/basics.rs
+++ src/basics.rs
@@ -1,22 +1,27 @@
 use std::env;
 
 fn main() {
-    println!("Hello, World!");
-    args()
+    let arguments: Vec<String> = env::args().collect();
+    let arg_count = arguments.len();
+    if arg_count == 2 {
+        let var = &arguments[1];  // type: &String | &str
+        args(var)
+    } else {
+        args("World!!");  // type: &str
+    }
 }
 
-fn args() {
+fn args(value: &str) {
     // Either use 'cargo run' or 'rustc src/basics.rs && ./basics'
-    println!("\nCommandLine argument section");
-    let args: Vec<String> = env::args().collect();
-    let program_name = &args[0];
-    println!("Invoker: '{}'", program_name);
-    looping()
+    println!("Hello, {}", value);
+    if !value.contains("World!!") {
+        println!("Heading to loops");
+        looping()
+    }
 }
 
 
 fn looping() {
-    println!("\nLooping section");
     for i in [1, 2, 3, 4] {
         println!("{}", i);
     }
