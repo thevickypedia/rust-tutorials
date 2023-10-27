c7531ec34d7301e6b5fdc0dbe09894e01d96cfee
thevickypedia <vignesh.sivanandarao@gmail.com>
2023-10-27 12:33:16 -0500

    Add pretty print with a JSON data

--- src/format/specifiers.rs
+++ src/format/specifiers.rs
@@ -1,8 +1,8 @@
 fn main() {
     default();
     debug();
-    precision()
-    // todo: implement pretty print for debug
+    precision();
+    pretty_print_debug();
 }
 
 fn default() {
@@ -18,4 +18,22 @@ fn debug() {
 fn precision() {
     let pi = std::f64::consts::PI;
     println!("Pi: {:.2}", pi);  // limits to 2 decimal points (3.14)
+}
+
+// Debug annotation for JSONData
+#[derive(Debug)]
+struct JSONData {
+    key: String,
+    value: u32,
+}
+
+fn pretty_print_debug() {
+    let json_data = JSONData {
+        key: "One".to_string(),
+        value: 1,
+    };
+    // placeholder to avoid the 'dead_code' warning (default)
+    //  this can also be overridden by setting '#[allow(dead_code)]'
+    println!("Key: {}, value: {}", json_data.key, json_data.value);
+    println!("Debug with Pretty-Print: {:#?}", json_data);
 }
