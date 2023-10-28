e135c86cf2594e339acc0433194ff47233e5f8ca
thevickypedia <vignesh.sivanandarao@gmail.com>
2023-10-28 08:00:53 -0500

    Parse the URL into parts

--- src/requests/get.rs
+++ src/requests/get.rs
@@ -1,13 +1,31 @@
 extern crate reqwest;
 
+use std::env::args;
+use std::process::exit;
 use reqwest::Url;
 
 fn main() {
     // if not blocking, method should be awaited 'reqwest.await.is_ok'
-    let request_url = "https://vigneshrao.com";
+    let arguments: Vec<String> = args().collect();
+    let request_url = &arguments.last().unwrap().to_string();
     if let Ok(url) = Url::parse(request_url) {
-        println!("URL OK");
-        println!("{}", url.as_str());
+        let url_str = url.as_str();
+        let scheme = url.scheme();
+        let host = url.host_str().unwrap();
+        let domain = url.domain().unwrap();
+        let path = url.path();
+        println!("URL OK: {}", url_str);
+        println!("scheme: {}", scheme);
+        if host == domain {
+            println!("host: {}", host);
+        } else {
+            println!("host: {}", host);
+            println!("domain: {}", domain);
+        }
+        println!("path: {}", path);
+    } else {
+        println!("'{}' is not a URL", request_url);
+        exit(1)
     }
     let resp = reqwest::blocking::get(request_url);
     if resp.is_ok() {
